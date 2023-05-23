64943b93ab322f6048c95268f5123e51ed6f4d80
Igor <nekto.korobko@gmail.com>
2023-05-23 17:24:48 +0300

    create model clients

--- beautycitybot/models.py
+++ beautycitybot/models.py
@@ -16,3 +16,53 @@ class UUIDMixin(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Clients(UUIDMixin, TimeStampedMixin):
+    telegram_id = models.IntegerField(unique=True)
+    username = models.CharField(
+        max_length=64,
+        null=True,
+        blank=False,
+        verbose_name='User Name'
+    )
+    first_name = models.CharField(
+        max_length=256,
+        null=True,
+        blank=False,
+        verbose_name='First Name'
+    )
+    last_name = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+        verbose_name='Last Name'
+    )
+    phone_number = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True,
+        verbose_name='Phone Number'
+    )
+    email = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True,
+        verbose_name='email'
+    )
+    is_admin = models.BooleanField(
+        null=True,
+        blank=True,
+        default=False,
+        verbose_name='Администратор'
+    )
+
+    def __str__(self):
+        if self.username:
+            return f'@{self.username}'
+        else:
+            return f'{self.telegram_id}'
+
+    class Meta:
+        verbose_name = 'Client'
+        verbose_name_plural = 'Clients'
